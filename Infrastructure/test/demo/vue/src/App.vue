b71bbd4cf58799e55029a264f61d59efba07242f
KieSun <zx597813039@gmail.com>
2019-09-19 22:45:19 +0800

    feat: add vue loading component

--- Infrastructure/test/demo/vue/src/App.vue
+++ Infrastructure/test/demo/vue/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Loading text="加载中" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Loading from "./components/loading.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Loading
   }
 };
 </script>
---
+++ Infrastructure/test/demo/vue/src/components/loading.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="loading">
+    <span class="loading__indicator" :style="style" />
+    <span class="loading__text" v-show="text">{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+    size: {
+      type: Number,
+      default: 26
+    },
+    indicatorColor: {
+      type: String,
+      default: "#1989FA"
+    }
+  },
+  computed: {
+    style() {
+      return {
+        width: this.size + "px",
+        height: this.size + "px",
+        borderColor: this.indicatorColor,
+        borderBottomColor: "transparent"
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.loading {
+  display: inline-flex;
+  align-items: center;
+
+  &__indicator {
+    border-radius: 100%;
+    margin: 2px;
+    border: 2px solid;
+    background: transparent !important;
+    display: inline-block;
+    animation: rotate 0.75s 0s linear infinite;
+  }
+
+  &__text {
+    margin-left: 10px;
+    font-size: 17px;
+
+    &--vertical {
+      margin-left: 0;
+      margin-top: 5px;
+    }
+  }
+}
+@keyframes rotate {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  50% {
+    -webkit-transform: rotate(180deg);
+    transform: rotate(180deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+</style>
